c2b0a2eef95687b900b069b95e678325b8209a0f
Arman Tarkhanian <atarkhanian@tynker.com>
2023-05-03 14:16:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Seller Onboarding Form
+
+This web application is a simple and user-friendly seller onboarding form that allows gift card sellers to provide their store details, gift card balance, selling price, network preference, and other necessary information. The form uses the Flask framework, and the results are stored in memory.
+
+## Files
+
+- `app.py`: The main Flask application file that handles routes and form processing.
+- `form.html`: The HTML template for the seller onboarding form, which includes client-side JavaScript for form validation and navigation between form questions.
+- `thank_you.html`: The HTML template with a thank you message for a submitted form, with a link to start a new entry.
+- `results.html`: The HTML template to show the table of results with proper bootstrap formatting based off stored local storage.
+
+## Features
+
+- Form validation with custom error messages.
+- Incremental form navigation with back and next buttons.
+- Saving form data in local storage to allow users to continue from where they left off.
+- User-friendly interface with Bootstrap styling.
+- Results page to view submitted forms.
+
+## Running the Application
+
+To run the application, follow these steps:
+
+1. Install the required dependencies (Flask):
+```
+pip install flask
+```
+2. Run the `app.py` file:
+```
+python app.py
+```
+3. Access the application in your web browser at `http://localhost:5001`.
+
+## Contributing
+
+Feel free to submit pull requests or open issues to contribute to this project.
---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import uuid
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.debug = True
+
+# This will store the results in memory
+results = []
+
+# UX improvements:
+# 1. Made it so you can press enter/return to go to the next question
+# 2. Made it so the text box is auto-selected when you press next
+# 3. Made the format of the results table less cluttered and easier to read via bootstrap styling
+# 4. Add a button on the thank you page to start a new entry, also made the thank you page a new route
+# 5. Added a "wipe data" button on the results.html page
+# 6. Made it so if you decide to change back to deposit after selecting valid, the gift card number and pin are removed
+
+
+@app.route('/', methods=['GET', 'POST'])
+def seller_onboarding_form():
+    if request.method == 'POST':
+        form_data = {
+            'store_name': request.form['store_name'],
+            'balance': request.form['balance'],
+            'selling_price': request.form['selling_price'],
+            'network': request.form['network'],
+            'receiving_address': request.form['receiving_address'],
+            'preference': request.form['preference'],
+            'card_number': request.form['card_number'],
+            'card_pin': request.form['card_pin'],
+            'email': request.form['email']
+        }
+        results.append(form_data)
+        return redirect('/thank-you')
+
+    return render_template('form.html')
+
+
+@app.route('/results')
+def results_page():
+    return render_template('results.html', results=results)
+
+
+@app.route('/thank-you')
+def thank_you():
+    return render_template('thank_you.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
